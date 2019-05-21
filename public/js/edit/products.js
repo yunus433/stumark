0881eb7c28b80818e751d1db5fdc4be09b903f86
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2019-05-21 07:31:04 +0300

    Fix /edit/products route  client side js issues

--- public/js/edit/products.js
+++ public/js/edit/products.js
@@ -1,14 +1,16 @@
 window.onload = () => {
+  const form = document.querySelector('.--s_left_side_wrapper');
+
   document.addEventListener('click', (event) => {
     if (event.target.className == '--s_product_image_change') {
       document.querySelector('.--s_product_image').src = event.target.src;
-      document.querySelector('.--s_product_image_form').action = "/edit/product/photo/?index=" + event.target.id.split("_")[1] + "&id=" + document.querySelector('.--s_product_image_form').action.split("=")[2];
+      form.action = "/edit/product/photo/?index=" + event.target.id.split("_")[1] + "&id=" + form.action.split("=")[2];
       document.querySelector('.--s_selected_photo').classList.remove('--s_selected_photo');
       event.target.classList.add('--s_selected_photo');
     };
   });
 
   document.querySelector('.--g_display_none').onchange = (event) => {
-    document.querySelector('.--s_left_side_wrapper').submit();
+    form.submit();
   };
 };
