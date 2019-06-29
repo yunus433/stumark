8a3e2076c2c723c7833412aedc515903621b23bb
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2019-06-29 09:51:13 +0300

    Make /sell/new route responsive

--- public/js/sell/messagesDashboard.js
+++ public/js/sell/messagesDashboard.js
@@ -8,13 +8,22 @@ window.onload = () => {
     window.location.href = '/buy/?page=0&category=all&limit=50&keywords=' + searchBar.childNodes[0].value;
   };
 
+  const userMenu = document.querySelector('.user-menu');
+  const userMenuResponsive = document.querySelector('.user-menu-responsive');
+  const contentBarResponsive = document.querySelector('.content-bar-responsive');
+
   document.addEventListener('mouseover', (event) => {
-    const userMenu = document.querySelector('.user-menu');
-    
     if (event.target.className == 'user-name-wrapper' || event.target.parentNode.className == 'user-name-wrapper' || event.target.className == 'user-menu' || event.target.parentNode.className == 'user-menu') {
       userMenu.style.display = 'flex';
+      userMenuResponsive.style.display = 'flex';
+      contentBarResponsive.style.display = 'none';
+    } else if (event.target.className == 'change-category-button' || event.target.parentNode.className == 'change-category-button' || event.target.className == 'content-bar-responsive' || event.target.parentNode.className == 'content-bar-responsive') {
+      contentBarResponsive.style.display = 'flex';
+      userMenuResponsive.style.display = 'none';
     } else {
+      contentBarResponsive.style.display = 'none';
       userMenu.style.display = 'none';
+      userMenuResponsive.style.display = 'none';
     }
   });
 };
--- public/js/sell/new.js
+++ public/js/sell/new.js
@@ -16,6 +16,7 @@ function createNewProductPhoto(imageSrc) {
   document.getElementById('file-name-array').value = productPhotoNameArray.join();
   document.querySelector('.images-wrapper').appendChild(imageWrapper);
   document.querySelector('.images-wrapper').insertBefore(imageWrapper, imageWrapper.previousElementSibling);
+  document.querySelector('.images-outer-wrapper').scrollLeft =  document.querySelector('.images-outer-wrapper').scrollWidth;
 }
 
 window.onload = () => {
@@ -28,13 +29,22 @@ window.onload = () => {
     window.location.href = '/buy/?page=0&category=all&limit=50&keywords=' + searchBar.childNodes[0].value;
   };
 
+  const userMenu = document.querySelector('.user-menu');
+  const userMenuResponsive = document.querySelector('.user-menu-responsive');
+  const contentBarResponsive = document.querySelector('.content-bar-responsive');
+
   document.addEventListener('mouseover', (event) => {
-    const userMenu = document.querySelector('.user-menu');
-    
     if (event.target.className == 'user-name-wrapper' || event.target.parentNode.className == 'user-name-wrapper' || event.target.className == 'user-menu' || event.target.parentNode.className == 'user-menu') {
       userMenu.style.display = 'flex';
+      userMenuResponsive.style.display = 'flex';
+      contentBarResponsive.style.display = 'none';
+    } else if (event.target.className == 'change-category-button' || event.target.parentNode.className == 'change-category-button' || event.target.className == 'content-bar-responsive' || event.target.parentNode.className == 'content-bar-responsive') {
+      contentBarResponsive.style.display = 'flex';
+      userMenuResponsive.style.display = 'none';
     } else {
+      contentBarResponsive.style.display = 'none';
       userMenu.style.display = 'none';
+      userMenuResponsive.style.display = 'none';
     }
   });
 
