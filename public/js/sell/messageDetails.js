03ec50b78401f9803e8ef5cb055a6f86ba93342c
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2019-06-29 21:57:59 +0300

    Fix opening menu problem on mobile deploy 2

--- public/js/sell/messageDetails.js
+++ public/js/sell/messageDetails.js
@@ -29,4 +29,18 @@ window.onload = () => {
       if (userMenuResponsive) userMenuResponsive.style.display = 'none';
     }
   });
+  document.addEventListener('touchstart', (event) => {
+    if (event.target.className == 'user-name-wrapper' || event.target.parentNode.className == 'user-name-wrapper' || event.target.className == 'user-menu' || event.target.parentNode.className == 'user-menu' || event.target.className == 'user-menu-responsive' || event.target.parentNode.className == 'user-menu-responsive') {
+      if (userMenu) userMenu.style.display = 'flex';
+      if (userMenuResponsive) userMenuResponsive.style.display = 'flex';
+      if (contentBarResponsive) contentBarResponsive.style.display = 'none';
+    } else if (event.target.className == 'change-category-button' || event.target.parentNode.className == 'change-category-button' || event.target.className == 'content-bar-responsive' || event.target.parentNode.className == 'content-bar-responsive') {
+      if (contentBarResponsive) contentBarResponsive.style.display = 'flex';
+      if (userMenuResponsive) userMenuResponsive.style.display = 'none';
+    } else {
+      if (contentBarResponsive) contentBarResponsive.style.display = 'none';
+      if (userMenu) userMenu.style.display = 'none';
+      if (userMenuResponsive) userMenuResponsive.style.display = 'none';
+    }
+  });
 };
