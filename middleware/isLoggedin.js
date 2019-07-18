9c848c0f6c33a28051d5eae4ac70c3c7627b4f79
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2019-07-18 22:14:40 +0300

    Fix small errors on /messages route

--- middleware/isLoggedin.js
+++ middleware/isLoggedin.js
@@ -1,6 +1,12 @@
+const User = require('../models/user/User');
+
 module.exports = (req, res, next) => {
   if (req.session && req.session.user) {
-    next();
+    User.findById(req.session.user._id, (err, user) => {
+      if (err) return res.redirect('/auth/login');
+      req.session.user = user;
+      next();
+    });
   } else {
     req.session.redirect = req.originalUrl;
     res.redirect('/auth/login');
