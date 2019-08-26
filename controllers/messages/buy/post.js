a3d866c0537d43a6388eef935520c6c5f96f131b
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2019-08-26 23:19:56 +0300

    -

--- controllers/messages/buy/post.js
+++ controllers/messages/buy/post.js
@@ -1,22 +1,31 @@
 const moment = require('moment');
+const mongoose = require('mongoose');
+
 const Message = require('../../../models/message/Message');
+const Product = require('../../../models/product/Product');
 
 module.exports = (req, res) => {
-  const newMessageData = {
-    content: req.body.message,
-    buyerId: req.session.user._id,
-    buyerName: req.session.user.name,
-    sendedBy: "buyer",
-    productId: req.query.id,
-    read: false,
-    createdAt: moment(Date.now()).format("[at] HH[:]mm A [/] DD[.]MM[.]YYYY")
-  };
-
-  const newMessage = new Message(newMessageData);
-
-  newMessage.save(err => {
+  Product.findById(mongoose.Types.ObjectId(req.query.id), (err, product) => {
     if (err) return res.redirect('/');
 
-    return res.redirect('/messages/buy/?id=' + req.query.id);
+    const newMessageData = {
+      content: req.body.message,
+      buyerId: req.session.user._id,
+      buyerName: req.session.user.name,
+      sendedBy: "buyer",
+      productId: req.query.id,
+      productName: product.name,
+      productProfile: product.productPhotoArray[0],
+      read: false,
+      createdAt: moment(Date.now()).format("[at] HH[:]mm A [/] DD[.]MM[.]YYYY")
+    };
+  
+    const newMessage = new Message(newMessageData);
+  
+    newMessage.save(err => {
+      if (err) return res.redirect('/');
+  
+      return res.redirect('/messages/buy/?id=' + req.query.id);
+    });
   });
 };
