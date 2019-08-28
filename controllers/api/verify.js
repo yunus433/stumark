95e693936fea995371369fabf85c7e300107b7ed
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2019-08-28 21:54:05 +0300

    -

--- controllers/api/verify.js
+++ controllers/api/verify.js
@@ -13,7 +13,7 @@ module.exports = (req, res) => {
       email: user.email,
       userId: user._id 
     }, 'userRegister', (err) => {
-      req.session.user = user;
+      if (err) return res.status(400).json({ "success": false });
 
       return res.status(200).json({ "success": true });
     });
