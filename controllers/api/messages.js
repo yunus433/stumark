8da9d5adeac1d1794f559a9a9863b96f0efdda19
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2019-10-10 21:36:33 +0300

    Change /api/messages route to update user not read messages

--- controllers/api/messages.js
+++ controllers/api/messages.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const _ = require('lodash');
 
 const Message = require('../../models/message/Message');
+const User = require('../../models/user/User');
 
 module.exports = (req, res) => {
   if (req.query && req.query.id) {
@@ -23,10 +24,16 @@ module.exports = (req, res) => {
         "read": false
       }, { $set: {
         read: true} 
-      }, {}, err => {
+      }, {}, (err, modifiedNum) => {
         if (err) return res.status(500).json({ "error": "Mongo Error: " + err });
 
-        return res.status(200).json({ messages });
+        User.findByIdAndUpdate(mongoose.Types.ObjectId(req.query.user), { $inc: {
+          "notReadMessage": -1 * modifiedNum
+        }}, {}, (err, user) => {
+          if (err) return res.status(500).json({ "error": "Mongo Error: " + err });
+
+          return res.status(200).json({ messages });
+        });
       });
     });
   } else if (req.query && req.query.buyer) {
