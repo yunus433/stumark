f10ca3c48cec8598cd1055a942feaafe7de04c85
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2019-10-12 18:05:56 +0300

    Fix /auth/register route problems

--- controllers/auth/register/post.js
+++ controllers/auth/register/post.js
@@ -5,40 +5,29 @@ const sendMail = require('../../../utils/sendMail');
 
 module.exports = (req, res, next) => {
   if (validator.isEmail(req.body.email)) {
-    req.gateway.customer.create({
+    const newUserData = {
       email: req.body.email,
-      firstName: req.body.name
-    }, function(err, result) {
-      if (err || !result.success) {
-        req.session.error = 'not valid email';
+      name: req.body.name,
+      university: req.body.university,
+      password: req.body.password
+    }; 
+
+    const newUser = new User(newUserData);
+
+    newUser.save((err, user) => {
+      if (err && err.code == 11000) {
+        req.session.error = 'already taken email';
         return res.redirect('/auth/register');
       }
+      if (err) return res.redirect('/');
 
-      const newUserData = {
-        email: req.body.email,
-        name: req.body.name,
-        university: req.body.university,
-        password: req.body.password,
-        customerPaymentId: result.customer.id
-      }; 
-  
-      const newUser = new User(newUserData);
-  
-      newUser.save((err, user) => {
-        if (err && err.code == 11000) {
-          req.session.error = 'already taken email';
-          return res.redirect('/auth/register');
-        }
-        if (err) return res.redirect('/');
-  
-        sendMail({
-          email: user.email,
-          userId: user._id 
-        }, 'userRegister', () => {
-          req.session.notVerifiedUser = user;
-  
-          return res.redirect('/auth/verify');
-        });
+      sendMail({
+        email: user.email,
+        userId: user._id 
+      }, 'userRegister', () => {
+        req.session.notVerifiedUser = user;
+
+        return res.redirect('/auth/verify');
       });
     });
   } else {
