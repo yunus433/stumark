61677fb8fd742939ad3673379ad59c1d3287223d
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2019-10-20 20:39:03 +0300

    Add remove from favorites

--- controllers/api/addToFavorites.js
+++ controllers/api/addToFavorites.js
@@ -6,11 +6,25 @@ module.exports = (req, res) => {
   if (!req.query || !req.query.id)
     return res.status(400).json({ "error": "Bad request" });
 
-  User.findByIdAndUpdate(mongoose.Types.ObjectId(req.query.id), { $push: {
-    "favorites": req.body.productId
-  }}, {new: true}, (err, user) => {
+  User.find(mongoose.Types.ObjectId(req.query.id), (err, user) => {
     if (err) return res.status(500).json({ "error": err });
 
-    return res.status(200).json({ "user": user })
+    if (user.favorites.includes(req.body.productId)) {
+      User.findByIdAndUpdate(mongoose.Types.ObjectId(req.query.id), { $pull: {
+        "favorites": req.body.productId
+      }}, {new: true}, (err, user) => {
+        if (err) return res.status(500).json({ "error": err });
+    
+        return res.status(200).json({ "user": user })
+      });
+    } else {
+      User.findByIdAndUpdate(mongoose.Types.ObjectId(req.query.id), { $push: {
+        "favorites": req.body.productId
+      }}, {new: true}, (err, user) => {
+        if (err) return res.status(500).json({ "error": err });
+    
+        return res.status(200).json({ "user": user })
+      });
+    }
   });
 };
