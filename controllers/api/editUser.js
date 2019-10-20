65f850a4e1f2b801acf5464f68e4b8f6dd307383
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2019-10-20 17:44:07 +0300

    -

--- controllers/api/editUser.js
+++ controllers/api/editUser.js
@@ -5,10 +5,10 @@ const User = require("../../models/user/User");
 
 module.exports = (req, res) => {
   if (!req.body || !req.body.email || !req.body.name || !req.body.university)
-    return res.status(400).json({"error: ": "Bad request"});
+    return res.status(400).json({"error": "Bad request"});
     
   if (!validator.isEmail(req.body.email))
-    return res.status(400).json({"error: ": "not valid email"});
+    return res.status(400).json({"error": "not valid email"});
 
   if (req.body.password) {
     User.findByIdAndUpdate(mongoose.Types.ObjectId(req.query.id), {$set: {
@@ -17,8 +17,8 @@ module.exports = (req, res) => {
       "email": req.body.email,
       "university": req.body.university
     }}, {new: true}, (err, user) => {
-      if (err && err.code == 11000) return res.status(400).json({"error: ": "email already taken"});
-      if (err) return res.status(500).json({"error: ": err});
+      if (err && err.code == 11000) return res.status(400).json({"error": "email already taken"});
+      if (err) return res.status(500).json({"error": err});
 
       return res.status(200).json({user});
     });
@@ -28,8 +28,8 @@ module.exports = (req, res) => {
       "email": req.body.email,
       "university": req.body.university
     }}, {new: true}, (err, user) => {
-      if (err && err.code == 11000) return res.status(400).json({"error: ": "email already taken"});
-      if (err) return res.status(500).json({"error: ": err});
+      if (err && err.code == 11000) return res.status(400).json({"error": "email already taken"});
+      if (err) return res.status(500).json({"error": err});
 
       return res.status(200).json({user});
     });
