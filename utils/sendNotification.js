980547c6a2e43ce1d374b9c29146462cc5475686
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2019-12-28 12:04:10 +0300

    -

--- utils/sendNotification.js
+++ utils/sendNotification.js
@@ -1,76 +1,77 @@
 const mongoose = require('mongoose');
-const { Expo } = require('expo-server-sdk');
+// const { Expo } = require('expo-server-sdk');
 
-const expo = new Expo();
+// const expo = new Expo();
 
 const User = require('../models/user/User');
 
 module.exports = (data, callback) => {
-  User.findById(mongoose.Types.ObjectId(data.to), (err, user) => {
-    if (err) return callback(err);
-    if (!user || !user.notificationToken) return callback("User not found");
+  return callback(false, "none");
+  // User.findById(mongoose.Types.ObjectId(data.to), (err, user) => {
+  //   if (err) return callback(err);
+  //   if (!user || !user.notificationToken) return callback("User not found");
 
-    const messages = [];
-    data.messages.forEach(message => {
-      messages.push({
-        to: data.to,
-        sound: 'default',
-        body: message.body,
-        data: { withSome: message.content },
-      });
-    });
+  //   const messages = [];
+  //   data.messages.forEach(message => {
+  //     messages.push({
+  //       to: data.to,
+  //       sound: 'default',
+  //       body: message.body,
+  //       data: { withSome: message.content },
+  //     });
+  //   });
 
-    let chunks = expo.chunkPushNotifications(messages);
-    let tickets = [];
-    (async () => {
-      for (let chunk of chunks) {
-        try {
-          let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-          tickets.push(...ticketChunk);
-        } catch (error) {
-          return callback(error);
-        }
-      }
-    })();
+  //   let chunks = expo.chunkPushNotifications(messages);
+  //   let tickets = [];
+  //   (async () => {
+  //     for (let chunk of chunks) {
+  //       try {
+  //         let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+  //         tickets.push(...ticketChunk);
+  //       } catch (error) {
+  //         return callback(error);
+  //       }
+  //     }
+  //   })();
 
-    let receiptIds = [];
-    for (let ticket of tickets) {
-      if (ticket.id) {
-        receiptIds.push(ticket.id);
-      }
-    }
+  //   let receiptIds = [];
+  //   for (let ticket of tickets) {
+  //     if (ticket.id) {
+  //       receiptIds.push(ticket.id);
+  //     }
+  //   }
     
-    let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
-    (async () => {
-      for (let chunk of receiptIdChunks) {
-        try {
-          let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+  //   let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+  //   (async () => {
+  //     for (let chunk of receiptIdChunks) {
+  //       try {
+  //         let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
 
-          for (let receipt of receipts) {
-            if (receipt.status === 'ok') {
-              continue;
-            } else if (receipt.status === 'error') {
-              if (receipt.details && receipt.details.error) {
-                User.findByIdAndUpdate(mongoose.Types.ObjectId(data.to), {$set: {
-                  notificationToken: null
-                }}, {}, (err, user) => {
-                  if (err) callback(err);
+  //         for (let receipt of receipts) {
+  //           if (receipt.status === 'ok') {
+  //             continue;
+  //           } else if (receipt.status === 'error') {
+  //             if (receipt.details && receipt.details.error) {
+  //               User.findByIdAndUpdate(mongoose.Types.ObjectId(data.to), {$set: {
+  //                 notificationToken: null
+  //               }}, {}, (err, user) => {
+  //                 if (err) callback(err);
       
-                  return callback(null, user);
-                });
-              }
-            }
-          }
-        } catch (error) {
-          User.findByIdAndUpdate(mongoose.Types.ObjectId(data.to), {$set: {
-            notificationToken: null
-          }}, {}, (err, user) => {
-            if (err) callback(err);
+  //                 return callback(null, user);
+  //               });
+  //             }
+  //           }
+  //         }
+  //       } catch (error) {
+  //         User.findByIdAndUpdate(mongoose.Types.ObjectId(data.to), {$set: {
+  //           notificationToken: null
+  //         }}, {}, (err, user) => {
+  //           if (err) callback(err);
 
-            return callback(null, user);
-          });
-        };
-      };
-    })();
-  });
+  //           return callback(null, user);
+  //         });
+  //       };
+  //     };
+  //   })();
+  // });
 }
