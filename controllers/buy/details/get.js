f34b909406bf9100f3655d989c74e63fe1a335a1
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-06-09 19:16:27 +0300

    -

--- controllers/buy/details/get.js
+++ controllers/buy/details/get.js
@@ -1,43 +1,57 @@
 const async = require('async');
+const mongoose = require('mongoose');
 
 const Product = require('../../../models/product/Product');
 const Chat = require('../../../models/chat/Chat');
 
 module.exports = (req, res, next) => {
   if (req.query && req.query.id) {
-    Product.findById(req.query.id, (err, product) => {
-      if (err) return res.redirect("/");
-
-      Product.getLatest(
-        {
-          category: JSON.parse(JSON.stringify(product)).category,
-          docsToSkip: 0,
-          limit: 9,
-          productId: product._id
-        },
-        (err, latestProducts) => {
-          if (err) return res.redirect("/");
-
-          async.times(
-            latestProducts.length,
-            (time, next) => {
-              Product.findById(latestProducts[time], (err, returnedProduct) => {
-                next(err, returnedProduct);
-              });
-            },
-            (err, similarProducts) => {
-              if (err) return res.redirect("/");
-
-              if (req.session.user) {
-                Chat.findOne({
-                  "buyer": req.session.user._id.toString(),
-                  "product": product._id.toString()
-                }, (err, chat) => {
-                  if (err) return res.redirect('/');
-
-                  if (chat) 
-                    return res.redirect('/messages/details?id=' + chat._id.toString());
-                    
+    try {
+      Product.findById(mongoose.Types.ObjectId(req.query.id), (err, product) => {
+        if (err || !product) return res.redirect("/");
+  
+        Product.getLatest(
+          {
+            category: JSON.parse(JSON.stringify(product)).category,
+            docsToSkip: 0,
+            limit: 9,
+            productId: product._id
+          },
+          (err, latestProducts) => {
+            if (err) return res.redirect("/");
+  
+            async.times(
+              latestProducts.length,
+              (time, next) => {
+                Product.findById(latestProducts[time], (err, returnedProduct) => {
+                  next(err, returnedProduct);
+                });
+              },
+              (err, similarProducts) => {
+                if (err) return res.redirect("/");
+  
+                if (req.session.user) {
+                  Chat.findOne({
+                    "buyer": req.session.user._id.toString(),
+                    "product": product._id.toString()
+                  }, (err, chat) => {
+                    if (err) return res.redirect('/');
+  
+                    if (chat) 
+                      return res.redirect('/messages/details?id=' + chat._id.toString());
+                      
+                    res.render('buy/details', {
+                      page: 'buy/details',
+                      title: product.name,
+                      includes: {
+                        external: ["css", "js", "fontawesome"]
+                      },
+                      product,
+                      similarProducts,
+                      user: req.session.user
+                    });
+                  });
+                } else {
                   res.render('buy/details', {
                     page: 'buy/details',
                     title: product.name,
@@ -45,26 +59,17 @@ module.exports = (req, res, next) => {
                       external: ["css", "js", "fontawesome"]
                     },
                     product,
-                    similarProducts,
-                    user: req.session.user
+                    similarProducts
                   });
-                });
-              } else {
-                res.render('buy/details', {
-                  page: 'buy/details',
-                  title: product.name,
-                  includes: {
-                    external: ["css", "js", "fontawesome"]
-                  },
-                  product,
-                  similarProducts
-                });
+                }
               }
-            }
-          );
-        }
-      );
-    });
+            );
+          }
+        );
+      });
+    } catch (err) {
+      return res.redirect('/');
+    }
   } else {
     res.redirect("/buy");
   }
