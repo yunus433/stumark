58bbe4c38dc37cbfab46b770632ad1f2689e937b
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-06-10 22:08:24 +0300

    -

--- controllers/auth/register/get.js
+++ controllers/auth/register/get.js
@@ -1,3 +1,6 @@
+const async = require('async');
+const mongoose = require('mongoose');
+
 const School = require('../../../models/school/School');
 
 module.exports = (req, res, next) => {
@@ -11,14 +14,27 @@ module.exports = (req, res, next) => {
   School.find({}, (err, schools) => {
     if (err) return res.redirect('/');
 
-    return res.render('auth/register', {
-      page: 'auth/register',
-      title: 'Kaydol',
-      includes: {
-        external: ['auth_css', 'js', 'fontawesome']
+    async.times(
+      schools.length,
+      (time, next) => {
+        School.findById(mongoose.Types.ObjectId(schools[time]._id), (err, school) => next(err, {
+          _id: school.id,
+          name: school.name
+        }));
       },
-      error,
-      schools
-    });
+      (err, schools) => {
+        if (err) return res.redirect('/');
+
+        return res.render('auth/register', {
+          page: 'auth/register',
+          title: 'Kaydol',
+          includes: {
+            external: ['auth_css', 'js', 'fontawesome']
+          },
+          error,
+          schools
+        });
+      }
+    );
   });
 }
