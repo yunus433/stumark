104e5f0b6b5ac1ac180d08fb6dc2ec6bf89879a1
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-06-11 23:35:41 +0300

    -

--- controllers/admin/index/post.js
+++ controllers/admin/index/post.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const User = require('../../../models/user/User');
 const Product = require('../../../models/product/Product');
 
+const engName = word => {
+  return word.toLocaleLowerCase().split('ş').join('s').split('ı').join('i').split('ö').join('o').split('ç').join('c').split('ü').join('u').split('ğ').join('g');
+}
+
 module.exports = (req, res) => {
   Product.find({}, (err, products) => {
     if (err) return res.redirect('/');
@@ -12,7 +16,7 @@ module.exports = (req, res) => {
       products.length,
       (time, next) => {
         Product.findByIdAndUpdate(mongoose.Types.ObjectId(products[time]._id), {$set: {
-          city: products[time].city_name ? products[time].city_name: "-"
+          city: products[time].city_name ? engName(products[time].city_name) : "-"
         }}, {}, err => next(err, true))
       },
       err => {
