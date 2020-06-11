2c6dfc486cafe5792493243de80bf8a28d1c17f7
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-06-11 23:06:25 +0300

    -

--- controllers/admin/index/post.js
+++ controllers/admin/index/post.js
@@ -4,6 +4,21 @@ const mongoose = require('mongoose');
 const User = require('../../../models/user/User');
 const Product = require('../../../models/product/Product');
 
+const subcategories = {
+  all: { all: "Tümü" },
+  book: { all: "Tümü", personal: "Kişisel Gelişim", novel: "Edebi Roman", history: "Tarih", foreign: "Yabancı Dil", poetry: "Şiir Kitabı", recipe: "Yemek Kitabı", psychology: "Psikoloji", business: "İş, Ekonomi, Hukuk" },
+  stationery: { all: "Tümü" },
+  electronic: { all: "Tümü" },
+  hobby: { all: "Tümü" },
+  fashion: { all: "Tümü" },
+  lesson: { all: "Tümü" },
+  exchange: { all: "Tümü" },
+  fun: { all: "Tümü" },
+  donation: { all: "Tümü" },
+  rented: { all: "Tümü" },
+  other: { all: "Tümü" }
+}
+
 module.exports = (req, res) => {
   Product.find({}, (err, products) => {
     if (err) return res.redirect('/');
@@ -12,7 +27,9 @@ module.exports = (req, res) => {
       products.length,
       (time, next) => {
         Product.findByIdAndUpdate(mongoose.Types.ObjectId(products[time]._id), {$set: {
-          price_number: (products[time].price == "ücretsiz" || products[time].price == "free" || products[time].price == "free₺" || products[time].price == "freé")? 0 : (products[time].price == "SATILDI" || products[time].price == "SOLD") ? -1 :  parseInt(products[time].price.split('₺').join('').split(',').join('.'))
+          subcategory: ['all', 'book', 'stationery', 'electronic', 'hobby', 'fashion', 'lesson', 'exchange', 'fun', 'donation', 'rented', 'other'].includes(products[time].category) ? subcategories[products[time].category][0] : "Tümü",
+          city: "-",
+          town: "-"
         }}, {}, err => next(err, true))
       },
       err => {
